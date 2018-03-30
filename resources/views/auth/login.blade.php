d850b4030b7406abba8e7c41baa4c41ff1b60e25
nothing628 <yogyphang@gmail.com>
2018-03-30 11:31:30 +0700

    fix login dialog

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,59 @@
-@extends('layouts.admin')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<div class="sixteen wide tablet six wide computer column">
+    <div class="ui left aligned segment">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+        <form id="form1" class="ui form" method="POST" action="{{ route('login') }}">
+            @csrf
+            <h1 class="ui header login center aligned">
+                <img src="{{asset('img/logo.png')}}" alt="aicmslogo" class="ui small image">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                <div class="content">
+                
+                </div>
+            </h1>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+            <div class="field">
+                <label>
+                    {{ __('E-Mail Address') }}:
+                </label>
+                <div class="ui fluid icon input">
+                    <input type="email" name="email" value="{{ old('email') }}" required autofocus>
+                    <i class="icon mail outline"></i>
                 </div>
             </div>
-        </div>
+            <div class="field">
+                <label>
+                    {{ __('Password') }}:
+                </label>
+                <div class="ui fluid icon input">
+                    <input type="password" name="password" style="width:100%!important;min-width:100%;width:100%;" required>
+                    <i class="icon key"></i>
+                </div>
+            </div>
+            <div class="field">
+                @if ($errors->has('email'))
+                    <span class="invalid-feedback">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+                @if ($errors->has('password'))
+                    <span class="invalid-feedback">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+                <button type="submit" class="ui teal right labeled icon button fluid">
+                    {{ __('Login') }}
+                    <i class="icon sign in"></i>
+                </button>
+
+                <a class="ui blue right labeled icon button fluid" href="{{ route('register') }}">
+                    Sign Up
+                    <i class="icon spy"></i>
+                </a>
+            </div>
+        </form>
     </div>
 </div>
 @endsection
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <title>@yield('title', 'AICMS')</title>
     <meta charset="utf-8" />
@@ -10,30 +9,29 @@
     <link href="{{ asset('css/ionicons.min.css') }}" rel="stylesheet" />
     <link href="{{ asset('css/admin.css') }}" rel="stylesheet" />
     <link href="{{ asset('css/pace.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
 </head>
-
 <body>
-    <div id="contextWrap">
-        @include('layouts.parts.leftmenu')
-        @include('layouts.parts.rightmenu')
-        <div class="pusher">
-            @include('layouts.parts.navbar')
-            <div class="mainWrap navslide">
-                <div class="ui equal width left aligned padded grid stackable">
-                    <!--Site Content-->
-
-                    @yield('content')
-                    <!--Site Content-->
-                </div>
+    <div class="ui container" style="padding-top:10%">
+        <div class="ui grid center aligned">
+            <div class="row">
+                @yield('content')
             </div>
         </div>
     </div>
+    
     <script src="{{asset('js/app.js')}}"></script>
     <script src="{{asset('js/semantic.min.js') }}"></script>
     <script src="{{asset('js/js.cookie-2.2.0.min.js')}}"></script>
     <script src="{{asset('js/jquery.nicescroll.min.js')}}"></script>
     <script src="{{asset('js/pace.js')}}" data-pace-options='{ "ajax": false }'></script>
     <script src="{{asset('js/admin.js')}}"></script>
+    <script>
+        var colors = ["#FFB700", "#FF6200", "#FF1C00",
+            "#00A9FF", "#54FFA9", "#B7FF46",
+            "#2a7fff", "#00d455", "#37c837"];
+        var rand = Math.floor(Math.random() * colors.length);
+        $('body').css("background-color", colors[rand]);
+    </script>
 </body>
-
 </html>
